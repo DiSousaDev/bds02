accc05b97601b9c486f4d1d826cff1b57d1fff5f
Eder Diego de Sousa <me@diego.dev.br>
2022-05-12 23:32:51 -0300

    Event tests passed

---
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -0,0 +1,32 @@
+package com.devsuperior.bds02.services;
+
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.repositories.EventRepository;
+import com.devsuperior.bds02.services.exceptions.DataNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class EventService {
+
+    @Autowired
+    private EventRepository repository;
+
+    @Transactional
+    public EventDTO update(Long id, EventDTO eventDTO) {
+        Event event = repository.findById(id).orElseThrow(() -> new DataNotFoundException("Evento não encontrado."));
+        copyDtoToEntity(eventDTO, event);
+        event = repository.save(event);
+        return new EventDTO(event);
+    }
+
+    private void copyDtoToEntity(EventDTO eventDTO, Event event) {
+        event.setName(eventDTO.getName());
+        event.setDate(eventDTO.getDate());
+        event.setUrl(eventDTO.getUrl());
+        event.setCity(new City(eventDTO.getCityId(), null));
+    }
+}
